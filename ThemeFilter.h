86994d9af06ff28462b48beab7838649f8473b6c
g4y4rwz6y8-glitch <g4y4rwz6y8@privaterelay.appleid.com>
2026-08-30 10:58:31 -0400

    feat: Enhance ThemeFilter header with screen-aware canvas detection and depth-limited font coloring

--- ThemeFilter.h
+++ ThemeFilter.h
@@ -3,8 +3,26 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface EeveeThemeFilter : NSObject
+
+// MARK: - Canvas & Card Detection
 + (BOOL)isBackgroundCanvas:(UIView *)view;
 + (BOOL)isCardOrCellSurface:(UIView *)view;
+
+// MARK: - Screen-Aware Canvas Finder
++ (UIView *)findDeepestBackgroundCanvasInView:(UIView *)view screenType:(NSString *)screenType;
+
+// MARK: - Safe Recursive Font Coloring
++ (void)recursivelyApplyFontColor:(UIColor *)color 
+                           toView:(UIView *)view 
+              skippingCardOrCellSurfaces:(BOOL)skipCards 
+                                 depth:(NSInteger)maxDepth;
+
+// MARK: - Blocklist Management
++ (NSSet<NSString *> *)blocklistKeywords;
++ (BOOL)classNameMatchesBlocklist:(NSString *)className;
++ (NSSet<NSString *> *)currentBlocklist;
++ (void)addToBlocklist:(NSString *)className;
+
 @end
 
 NS_ASSUME_NONNULL_END
