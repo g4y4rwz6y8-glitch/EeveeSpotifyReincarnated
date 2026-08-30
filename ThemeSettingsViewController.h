8be6cd3c6bc36edb2593be06d5fc42788ccee4a9
g4y4rwz6y8-glitch <g4y4rwz6y8@privaterelay.appleid.com>
2026-08-30 10:12:52 -0400

    Create ThemeSettingsViewController.h

---
+++ ThemeSettingsViewController.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+#import <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
+#import <PhotosUI/PhotosUI.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ThemeSettingsViewController : UITableViewController <UIDocumentPickerDelegate, PHPickerViewControllerDelegate>
+@end
+
+NS_ASSUME_NONNULL_END
