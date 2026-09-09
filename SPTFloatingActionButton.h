a1b42acb9f9fae626e2c061aabb325bd42547304
g4y4rwz6y8-glitch <g4y4rwz6y8@privaterelay.appleid.com>
2026-09-09 08:13:59 -0400

    Add eligibility check method for content window

--- SPTFloatingActionButton.h
+++ SPTFloatingActionButton.h
@@ -1,6 +1,9 @@
 #import <UIKit/UIKit.h>
 
 @interface SPTFloatingActionButton : UIButton
+
 + (instancetype)sharedButton;
++ (BOOL)isEligibleContentWindow:(UIWindow *)window; // Add this line
 - (void)installInWindow:(UIWindow *)window;
+
 @end
